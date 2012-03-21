e59b46ed1d70fd3b5ce6740d930f6b3128e56c5f
Christoph Heer <Christoph.Heer@gmail.com>
2012-03-21 09:44:25 +0100

    Add url converter for bson ObjectId from flask-mongokit

---
+++ test/test_url_converter.py
@@ -0,0 +1,16 @@
+import util
+from flask.ext.pymongo import BSONObjectIdConverter
+
+from bson import ObjectId
+from werkzeug.exceptions import BadRequest
+
+class UrlConverterTest(util.FlaskPyMongoTest):
+
+    def test_bson_object_id_converter(self):
+        converter = BSONObjectIdConverter("/")
+        
+        self.assertRaises(BadRequest, converter.to_python, ("132"))
+        assert converter.to_python("4e4ac5cfffc84958fa1f45fb") == \
+               ObjectId("4e4ac5cfffc84958fa1f45fb")
+        assert converter.to_url(ObjectId("4e4ac5cfffc84958fa1f45fb")) == \
+               "4e4ac5cfffc84958fa1f45fb"
