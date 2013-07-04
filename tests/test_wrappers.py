ee1e17ee2f3ea7137f097f6277f9b698dd71f3f6
Dan Crosta <dcrosta@late.am>
2013-07-04 11:08:58 -0400

    Py3k test compatibility

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -8,7 +8,7 @@ class CollectionTest(util.FlaskPyMongoTest):
 
         try:
             self.mongo.db.things.find_one_or_404({'_id': 'thing'})
-        except HTTPException, notfound:
+        except HTTPException as notfound:
             assert notfound.code == 404, "raised wrong exception"
 
         self.mongo.db.things.insert({'_id': 'thing', 'val': 'foo'}, safe=True)
@@ -22,7 +22,7 @@ class CollectionTest(util.FlaskPyMongoTest):
         self.mongo.db.things.morethings.remove()
         try:
             self.mongo.db.things.morethings.find_one_or_404({'_id': 'thing'})
-        except HTTPException, notfound:
+        except HTTPException as notfound:
             assert notfound.code == 404, "raised wrong exception"
 
         self.mongo.db.things.morethings.insert({'_id': 'thing', 'val': 'foo'}, safe=True)
