475e7a9d045c94fad849ec106c05890702f8443a
Dan Crosta <dcrosta@late.am>
2015-04-28 22:07:02 -0400

    whitespace

--- tests/test_url_converter.py
+++ tests/test_url_converter.py
@@ -8,7 +8,7 @@ class UrlConverterTest(util.FlaskPyMongoTest):
 
     def test_bson_object_id_converter(self):
         converter = BSONObjectIdConverter("/")
-        
+
         self.assertRaises(BadRequest, converter.to_python, ("132"))
         assert converter.to_python("4e4ac5cfffc84958fa1f45fb") == \
                ObjectId("4e4ac5cfffc84958fa1f45fb")
