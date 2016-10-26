b6389347530eae934912214a1a553317ab181011
sam <sam@untapt.com>
2016-10-25 23:07:35 -0400

    remove underscore

--- flask_pymongo/__init__.py
+++ flask_pymongo/__init__.py
@@ -131,7 +131,7 @@ class PyMongo(object):
             if not parsed.get('database'):
                 raise ValueError('MongoDB URI does not contain database name')
             app.config[key('DBNAME')] = parsed['database']
-            app.config[key('READ_PREFERENCE')] = parsed['options'].get('read_preference')
+            app.config[key('READ_PREFERENCE')] = parsed['options'].get('readpreference')
             app.config[key('USERNAME')] = parsed['username']
             app.config[key('PASSWORD')] = parsed['password']
             app.config[key('REPLICA_SET')] = parsed['options'].get('replicaset')
