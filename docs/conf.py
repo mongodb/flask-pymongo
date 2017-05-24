de051b2133be2b9a3abe24f4a137e2235c0bc81b
Dan Crosta <dcrosta@late.am>
2017-05-24 06:27:52 -0700

    update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask-PyMongo'
-copyright = u'2011 - 2015, Dan Crosta'
+copyright = u'2011 - 2017, Dan Crosta'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
