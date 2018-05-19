365bd330c1cdc3672d798698d717779a3775b58e
Dan Crosta <dcrosta@late.am>
2018-05-19 14:06:12 -0400

    cut 0.5.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Flask-PyMongo',
-    version='0.5.1',
+    version='0.5.2',
     url='http://flask-pymongo.readthedocs.org/',
     download_url='https://github.com/dcrosta/flask-pymongo/tags',
     license='BSD',
