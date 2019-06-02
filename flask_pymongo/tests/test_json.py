5fda54b31e37e1ab16d712a484203cafcafbcacb
Dan Crosta <dcrosta@late.am>
2019-06-02 16:50:13 -0400

    python 3.5 test compatibility fix

--- flask_pymongo/tests/test_json.py
+++ flask_pymongo/tests/test_json.py
@@ -2,6 +2,7 @@ import json
 
 from bson import ObjectId
 from flask import jsonify
+from six import ensure_str
 
 from flask_pymongo.tests.util import FlaskPyMongoTest
 
@@ -10,12 +11,12 @@ class JSONTest(FlaskPyMongoTest):
 
     def test_it_encodes_json(self):
         resp = jsonify({"foo": "bar"})
-        dumped = json.loads(resp.get_data())
+        dumped = json.loads(ensure_str(resp.get_data()))
         self.assertEqual(dumped, {"foo": "bar"})
 
     def test_it_handles_pymongo_types(self):
         resp = jsonify({"id": ObjectId("5cf29abb5167a14c9e6e12c4")})
-        dumped = json.loads(resp.get_data())
+        dumped = json.loads(ensure_str(resp.get_data()))
         self.assertEqual(dumped, {"id": {"$oid": "5cf29abb5167a14c9e6e12c4"}})
 
     def test_it_jsonifies_a_cursor(self):
@@ -24,5 +25,5 @@ class JSONTest(FlaskPyMongoTest):
         curs = self.mongo.db.rows.find(projection={"_id": False}).sort("foo")
 
         resp = jsonify(curs)
-        dumped = json.loads(resp.get_data())
+        dumped = json.loads(ensure_str(resp.get_data()))
         self.assertEqual([{"foo": "bar"}, {"foo": "baz"}], dumped)
